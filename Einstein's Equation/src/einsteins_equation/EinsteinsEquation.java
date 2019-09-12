65caa324f1f5117b05678b10ff99151d0aa043d1
Johnathan Meloche <johnathan.meloche@ocsbstudent.ca>
2019-09-12 13:10:36 -0400

    Finished Console Version of Day 5

---
+++ Einstein's Equation/src/einsteins_equation/EinsteinsEquation.java
@@ -0,0 +1,33 @@
+/*
+ * Created by: Johnathan Meloche
+ * Created on: 09/12/19
+ * Created for: ICS4U
+ * Day #5 (Einstein's Equation)
+ * This program Calculates stuff :)
+*/
+package einsteins_equation;
+import java.util.Scanner;
+public class EinsteinsEquation {
+	
+	public static void main(String[] args) {
+		//SPEED OF LIGHT
+		final double c = 299792458;
+		//Energy
+		double E;
+		//Mass
+		double m;
+
+
+		//Prompt
+		System.out.println("Einsteins Equation Calculator");
+		System.out.print("Input the mass in KG: ");
+		
+		
+		Scanner value = new Scanner(System.in);
+		m = value.nextDouble();
+		E = m * c *c;
+		System.out.println(Double.toString(E) + " Joules");
+		
+	}
+
+}
