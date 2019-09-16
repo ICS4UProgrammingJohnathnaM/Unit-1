169d6f2e6f218d44e98d880a63b6cf62ba4dbd8c
Johnathan Meloche <johnathan.meloche@ocsbstudent.ca>
2019-09-16 12:10:10 -0400

    Finished Day 6

--- Simple Calculator/src/einsteins_equation/SimpleCalulatorGUI.java
+++ Simple Calculator/src/einsteins_equation/SimpleCalulatorGUI.java
@@ -1,3 +1,10 @@
+/*
+ * Created by: Johnathan Meloche
+ * Created on: 09/16/2019
+ * Created for: ICS4U
+ * Day #6 (Simple Calculator)
+ * This program is a calculator with 2 number inputs and 4 simple operands
+*/
 package einsteins_equation;
 
 import java.awt.EventQueue;
@@ -43,10 +50,9 @@ public class SimpleCalulatorGUI {
 	 */
 	private void initialize() {
 		//Setting Calculator Variables
-		String operator = "+";
-		double number1 = 0;
-		double number2 = 0;
-		String AnswerText = "";
+		String[] operator = new String[1];
+		double[] number = new double[2];
+		String[] answerText = new String[1];
 		
 		
 		frame = new JFrame();
@@ -57,7 +63,7 @@ public class SimpleCalulatorGUI {
 		JButton btnMultiply = new JButton(" X ");
 		btnMultiply.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				operator = "*";
+				operator[0] = "*";
 			}
 		});
 		btnMultiply.setBounds(245, 32, 79, 73);
@@ -66,7 +72,7 @@ public class SimpleCalulatorGUI {
 		JButton btnDivison = new JButton(" ÷ ");
 		btnDivison.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				operator = "/";
+				operator[0] = "/";
 			}
 		});
 		btnDivison.setBounds(245, 116, 79, 72);
@@ -75,7 +81,7 @@ public class SimpleCalulatorGUI {
 		JButton btnAddition = new JButton(" + ");
 		btnAddition.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				operator = "+";
+				operator[0] = "+";
 			}
 		});
 		btnAddition.setBounds(245, 200, 79, 73);
@@ -84,58 +90,58 @@ public class SimpleCalulatorGUI {
 		JButton btnSubtraction = new JButton(" - ");
 		btnSubtraction.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				operator = "-";
+				operator[0] = "-";
 			}
 		});
 		btnSubtraction.setBounds(245, 282, 79, 72);
 		frame.getContentPane().add(btnSubtraction);
 		
 		txtFirstnumber = new JTextField();
-		txtFirstnumber.setText("firstNumber");
+		txtFirstnumber.setText("");
 		txtFirstnumber.setBounds(76, 85, 86, 20);
 		frame.getContentPane().add(txtFirstnumber);
 		txtFirstnumber.setColumns(10);
 		
 		txtSecondnumber = new JTextField();
-		txtSecondnumber.setText("secondNumber");
+		txtSecondnumber.setText("");
 		txtSecondnumber.setBounds(76, 168, 86, 20);
 		frame.getContentPane().add(txtSecondnumber);
 		txtSecondnumber.setColumns(10);
 		
 		JLabel lblFirstNumber = new JLabel("First Number");
-		lblFirstNumber.setBounds(83, 55, 79, 27);
+		lblFirstNumber.setBounds(83, 55, 98, 27);
 		frame.getContentPane().add(lblFirstNumber);
 		
 		JLabel lblSecondNumber = new JLabel("Second Number");
-		lblSecondNumber.setBounds(86, 126, 79, 27);
+		lblSecondNumber.setBounds(83, 130, 98, 27);
 		frame.getContentPane().add(lblSecondNumber);
 		
 		JLabel lblAnswer = new JLabel("Answer");
-		lblAnswer.setBounds(86, 292, 46, 14);
+		lblAnswer.setBounds(83, 282, 57, 27);
 		frame.getContentPane().add(lblAnswer);
 		
 		JButton btnCalculate = new JButton("Calculate");
 		btnCalculate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				number1 = Double.parseDouble(txtFirstnumber.getText());
-				number2 = Double.parseDouble(txtSecondnumber.getText());
+				number[0] = Double.parseDouble(txtFirstnumber.getText());
+				number[1] = Double.parseDouble(txtSecondnumber.getText());
 				
-				if (operator == "+") {
-					AnswerText = Double.toString(number1 + number2);
+				if (operator[0] == "+") {
+					answerText[0] = Double.toString(number[0] + number[1] );
 				}
-				else if (operator == "*") {
-					AnswerText = Double.toString(number1 * number2);
+				else if (operator[0] == "*") {
+					answerText[0] = Double.toString(number[0] * number[1] );
 				}
-				else if (operator == "/") {
-					AnswerText = Double.toString(number1 / number2);
+				else if (operator[0] == "/") {
+					answerText[0] = Double.toString(number[0] / number[1] );
 				}
-				else if (operator == "-") {
-					AnswerText = Double.toString(number1 - number2);
+				else if (operator[0] == "-") {
+					answerText[0] = Double.toString(number[0] - number[1] );
 				}
 				else {
-					AnswerText = "ERROR!";
+					answerText[0] = "ERROR!";
 				}
-				lblAnswer.setText(AnswerText);
+				lblAnswer.setText(answerText[0]);
 			}
 		});
 		btnCalculate.setBounds(57, 217, 127, 38);
